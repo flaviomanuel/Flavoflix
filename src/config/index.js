7edb49ae2200a7e17d36cf6f16cbf9cdb5088b40
unknown <flaviomanuel258@gmail.com>
2020-08-03 14:15:28 -0300

    correcting

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'http://flavoflix.herokuapp.com';
+  : 'https://flavoflix.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
